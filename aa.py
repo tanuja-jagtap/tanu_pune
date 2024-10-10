e7f4ac311c7b9228fe2411c3bd6d9c3e8fb960e6
tanujajagtap <kaletanuja80@gmail.com>
2024-10-10 14:24:20 +0530

    new content add

--- aa.py
+++ aa.py
@@ -1,3 +1,12 @@
 ghjhj
 jhjhjhk
 sjhdjdjn
+jhjkjkdjkjdkj
+def decor(fun):
+    def inner():
+        print("*"*20)
+        fun()
+        print("*"*20)
+    return inner()
+def show():
+    print("hii tanuja")
